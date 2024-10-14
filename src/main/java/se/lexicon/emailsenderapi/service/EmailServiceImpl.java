e6fe1f82695b422a3e1ebc19174d07600228328b
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-14 17:45:20 +0200

    Implemented

--- src/main/java/se/lexicon/emailsenderapi/service/EmailServiceImpl.java
+++ src/main/java/se/lexicon/emailsenderapi/service/EmailServiceImpl.java
@@ -1,4 +1,9 @@
 package se.lexicon.emailsenderapi.service;
 
+import se.lexicon.emailsenderapi.domain.dto.EmailDTO;
+
 public class EmailServiceImpl {
+    void sendEmail(EmailDTO emailDTO) {
+
+    }
 }
